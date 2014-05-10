e79213b18ebd0f8fff7cfbc332576efdb7c89940
Andrea Fiore <andrea.fiore@composable.io>
2014-05-10 17:43:37 +0100

    Bump spray and akka versions

--- project/Build.scala
+++ project/Build.scala
@@ -5,25 +5,25 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.lastroundapp",
     version := "1.0-SNAPSHOT",
-    scalaVersion := "2.10.3",
+    scalaVersion := "2.10.4",
     scalacOptions ++= Seq("-Xlint", "-deprecation", "-unchecked", "-feature", "-encoding", "utf8"),
     scalacOptions in Test ++= Seq("-Yrangepos")
   )
 }
 
 object Dependencies {
-  val akkaVersion  = "2.2.3"
-  val sprayVersion = "1.2.0"
+  val akkaVersion  = "2.3.2"
+  val sprayVersion = "1.3.2-20140428"
   val sprayCan     = "io.spray" % "spray-can" % sprayVersion
   val sprayClient  = "io.spray" % "spray-client" % sprayVersion
   val sprayRouting = "io.spray" % "spray-routing" % sprayVersion
-  val sprayJson    = "io.spray" %% "spray-json" % "1.2.5"
+  val sprayJson    = "io.spray" %% "spray-json" % "1.2.6"
   val sprayTest    = "io.spray" % "spray-testkit" % sprayVersion % "test"
   val akkaActor    = "com.typesafe.akka" %% "akka-actor" % akkaVersion
   val akkaSlf4j    = "com.typesafe.akka"   %%  "akka-slf4j" % akkaVersion
   val akkaTestkit  = "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test"
   val scalaLogging = "com.typesafe" %% "scalalogging-slf4j" % "1.0.1"
-  val logback      = "ch.qos.logback" % "logback-classic" % "1.0.13"
+  val logback      = "ch.qos.logback" % "logback-classic" % "1.1.2"
   val specs2       = "org.specs2" %% "specs2" % "2.3.11" % "test"
   val mockito      = "org.mockito" % "mockito-core" % "1.9.5" % "test"
   val commonDeps   = Seq(
